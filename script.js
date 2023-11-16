9f9fd34be4eb89eaab42b825bb72976c58c4ff4b
Idhant Jena <ijena@uci.edu>
2023-11-16 01:41:13 -0800

    Made a card and a price slider

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Execute code after the DOM is fully loaded
     console.log('DOM Loaded');
-});
+
+})
+
+var slider = document.getElementById("range");
+var output = document.getElementById("value");
+output.innerHTML = slider.value;
+
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
