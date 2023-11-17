f147261b3821383f74d9c20380210c8ee00f4008
Ha <muninnhugin@gmail.com>
2023-11-17 11:30:54 -0800

    add draft for search function

--- helper_scripts/listingCards.js
+++ helper_scripts/listingCards.js
@@ -1,6 +1,4 @@
-function createListingCard(index) {
-    let listing = listings[index]
-
+function createListingCard(listing) {
     let name = document.createElement('h2');
     name.innerText = listing.name;
     let price = document.createElement('h4');
@@ -16,23 +14,33 @@ function createListingCard(index) {
     return card;
 }
 
-function createListingsContainer(start, end, type) {
+function createListingsContainer(listings)
+{
     let listingsContainer = document.createElement('div');
     listingsContainer.className = "listingsContainer"
 
-    for(let i = start; i < end; ++i) {
-        if(listings[i]['type'] === type) {
-            listingsContainer.append(createListingCard(i));
-        }
+    for(let listing of listings) {
+        listingsContainer.append(createListingCard(listing));
     }
 
     return listingsContainer;
 }
 
+function createListingsContainerByIndex(start, end, type) {
+    let listingsContainer = document.createElement('div');
+    listingsContainer.className = "listingsContainer"
+
+    let data = listings.slice(start, end).filter((listing) => {
+        return listing['type'] === type;
+    })
+
+    return createListingsContainer(data);
+}
+
 function createBuyListingsContainer(start, end) {
-    return createListingsContainer(start, end, 'buy');
+    return createListingsContainerByIndex(start, end, 'buy');
 }
 
 function createBidListingsContainer(start, end) {
-    return createListingsContainer(start, end, 'bid');
+    return createListingsContainerByIndex(start, end, 'bid');
 }
